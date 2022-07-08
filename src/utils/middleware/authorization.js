11cc9884c2dbd5a4d1a1aa6d2b365bb8ec77eda7
Ana Valdemoro <ana_v_r97@hotmail.com>
2022-07-08 23:42:38 +0100

    FTR: create checkUserIsAuthorized to protect the routes of the application

---
+++ src/utils/middleware/authorization.js
@@ -0,0 +1,35 @@
+import boom from "@hapi/boom";
+import logger from "../../config/winston";
+import { isUserAuthorized } from "../../features/api/users/users.service";
+
+export const checkUserIsAuthorized = (resource) => async (req, res, next) => {
+  const { user } = req;
+  console.log("soy el user del checkUserAuthorized");
+  console.log(user);
+
+  try {
+    const hasPermission = await isUserAuthorized(user, resource);
+
+    if (hasPermission) {
+      logger.info(
+        `User "${user.full_name}" has authorization to "${req.baseUrl}${req.url}"`
+      );
+      return next();
+    }
+    logger.error(
+      `User "${user.full_name}" has no authorization to "${req.baseUrl}${req.url}"`
+    );
+    return next(
+      boom.forbidden(
+        `User "${user.full_name}" has no authorization to "${req.baseUrl}${req.url}"`
+      )
+    );
+  } catch (error) {
+    logger.error(`${error}`);
+    return next(
+      boom.forbidden(
+        `User "${user.full_name}" has no authorization to "${req.baseUrl}${req.url}"`
+      )
+    );
+  }
+};
