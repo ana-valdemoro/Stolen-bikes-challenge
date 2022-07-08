4f700e625e4e55d49797075f88f2cb3608686add
Ana Valdemoro <ana_v_r97@hotmail.com>
2022-07-08 12:46:47 +0100

    CLP: added definition to method getPafinationParams

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -1,6 +1,14 @@
 import config from "../config/index";
 
-const getParams = (params) => {
+/**
+ *
+ * @param {object} params contains all the params inside the query string route
+ * @returns {object} with the following format:
+ *  - limit: null | limit,  number of items to find
+ *  - page:  null |  number,  means the current page
+ */
+
+const getPaginationParams = (params) => {
   const { page, pageSize } = params;
   let limit = parseInt(pageSize, 10) || config.pagination.pageSize;
   if (limit > config.pagination.maxPageSize) {
@@ -18,4 +26,4 @@ const getParams = (params) => {
   };
 };
 
-export default getParams;
+export default getPaginationParams;
