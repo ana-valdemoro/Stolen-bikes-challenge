fbba0efd620ceac7100189adb9a7aadde69ce7bc
Ana Valdemoro <ana_v_r97@hotmail.com>
2022-07-14 11:44:43 +0100

    FIX: delete userRouter

--- src/features/api.router.js
+++ src/features/api.router.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import swaggerUi from "swagger-ui-express";
-import userRouter from "./api/users/users.router";
 import authRouter from "./api/auth/auth.router";
 import stolenBikesRouter from "./api/stolenBike/stolenBike.router";
 import departmentRouter from "./api/deparment/department.router";
 import policeOfficerRouter from "./api/policeOfficer/policeOfficer.router";
 import { authorizeHeader } from "../utils/middleware/jwt";
-import { hasDirectorPermissions } from "../utils/middleware/authorization";
 import config from "../config/index";
 import { swaggerSpec } from "../config/swagger";
 
@@ -22,12 +20,7 @@ if (config.env !== "production") {
 }
 
 router.use(`${baseRoute}auth`, authRouter);
-router.use(
-  `${baseRoute}users`,
-  authorizeHeader,
-  hasDirectorPermissions,
-  userRouter
-);
+
 router.use(`${baseRoute}stolen-bikes`, authorizeHeader, stolenBikesRouter);
 router.use(`${baseRoute}departments`, authorizeHeader, departmentRouter);
 router.use(`${baseRoute}police-officers`, authorizeHeader, policeOfficerRouter);
